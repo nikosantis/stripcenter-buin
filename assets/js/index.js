e942ee7b7a573c0f5ee3a75671a8e1f65891fd8c
Nikolas Santis <nikosantis@gmail.com>
2020-01-29 22:32:47 -0300

    initial commit!

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,68 @@
 const slider = tns({
   container: '.my-slider',
-  'items': 2,
-  'center': true,
-  'loop': false,
-  'swipeAngle': false,
-  'speed': 400,
-  'mouseDrag': true,
-  'gutter': 30,
-  'loop': true,
-  'navPosition': 'bottom',
-  'controls': false
+  items: 2,
+  center: true,
+  swipeAngle: false,
+  speed: 400,
+  mouseDrag: true,
+  gutter: 30,
+  loop: true,
+  navPosition: 'bottom',
+  controls: false,
+  speed: 300,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayTimeout: 3500,
+  autoplayButtonOutput: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    768: {
+      items: 2
+    }
+  }
 });
 
-let tabsWithContent = (function () {
-  let tabs = document.querySelectorAll('.tabs li');
-  let tabsContent = document.querySelectorAll('.tab-content');
+let tabsWithContent1 = (function () {
+  let tabs = document.querySelectorAll('#tab1 li');
+  let tabsContent = document.querySelectorAll('#contentTab1');
+
+  let deactvateAllTabs = function () {
+    tabs.forEach(function (tab) {
+      tab.classList.remove('is-active');
+    });
+  };
+
+  let hideTabsContent = function () {
+    tabsContent.forEach(function (tabContent) {
+      tabContent.classList.remove('is-active');
+    });
+  };
+
+  let activateTabsContent = function (tab) {
+    tabsContent[getIndex(tab)].classList.add('is-active');
+  };
+
+  let getIndex = function (el) {
+    return [...el.parentElement.children].indexOf(el);
+  };
+
+  tabs.forEach(function (tab) {
+    tab.addEventListener('click', function () {
+      deactvateAllTabs();
+      hideTabsContent();
+      tab.classList.add('is-active');
+      activateTabsContent(tab);
+    });
+  })
+
+  tabs[0].click();
+})();
+
+let tabsWithContent2 = (function () {
+  let tabs = document.querySelectorAll('#tab2 li');
+  let tabsContent = document.querySelectorAll('#contentTab2');
 
   let deactvateAllTabs = function () {
     tabs.forEach(function (tab) {
@@ -71,3 +119,29 @@ window.onscroll = function(e) {
     stuck = false;
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
